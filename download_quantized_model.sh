70639c525db739f38ff9e03953dce3ccb59d936e
mckinlde <douglas.e.mckinley@gmail.com>
2025-06-02 12:32:09 -0700

    time to download a model

--- download_quantized_model.sh
+++ download_quantized_model.sh
@@ -1,6 +1,15 @@
-# Create a models directory
+#!/usr/bin/env bash
+
+# Create a models directory if it doesn't exist
 mkdir -p ~/experiments/local_llms/models
+cd ~/experiments/local_llms/
+[ -d models ] || mkdir models
+cd models
 
 # Download a small GGUF model (e.g., TinyLlama 1.1B)
-cd ~/experiments/local_llms/models
-wget https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-GGUF/resolve/main/tinyllama-1.1b-chat.q4_K_M.gguf
+# Read the token from read_token.txt (make sure read_token.txt contains only the token)
+HUGGINGFACE_TOKEN=$(<read_token.txt)
+
+MODEL_URL="https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-GGUF/resolve/main/tinyllama-1.1b-chat.q4_K_M.gguf"
+
+wget --header="Authorization: Bearer $HUGGINGFACE_TOKEN" "$MODEL_URL"
